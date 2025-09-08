afb52672e4fc7ee8ef73d3cb62e0c735fa94ff81
Himanshu Dilip Bhajikhaye <bhajikhayehimanshu8@gmail.com>
2025-09-08 11:44:20 +0530

    Initial Commit

---
+++ Sum.java
@@ -0,0 +1,20 @@
+ --Sum of number--
+
+package com.company;
+
+//code for summation of two numbers
+
+public class Sum{
+
+  public static void main (String[] args) {
+    
+      //taking two variables
+
+      int a = 8;
+      int b = 3;
+
+      //displaying output directly of two numbers
+
+      System.out.println(a+b);
+   }
+}
