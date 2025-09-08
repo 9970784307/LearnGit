c7bfd7a5c5bdb06b0120ab47c3a3d273b478985c
Himanshu Dilip Bhajikhaye <bhajikhayehimanshu8@gmail.com>
2025-09-08 11:52:36 +0530

    added Multiply feature by me

---
+++ Multiply.java
@@ -0,0 +1,21 @@
+--product of number--
+
+package com.company;
+
+//code to show the multiplication of the given two variables numbers
+
+public class Multiply{
+
+  public static void main(String[] args){
+
+      //two variables taking manually
+
+      int x = 4;
+      int y = 8;
+
+      //showing the product of these two numbers
+
+      System.out.println("The product is: " + x*y);
+
+   }
+} 
